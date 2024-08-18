5a7d77b29e4908f0cec8fc2030cb71efb153a38a
Ankura <Master-Anku.com>
2024-08-18 14:53:12 +0630

    uploade the Iteration

---
+++ 1.Activites/2.Iteration/script.js
@@ -0,0 +1,10 @@
+// အမည်ပေး၍ ကျောင်းသားများကို သတ်မှတ်ချင်း
+var students = ['U Agga', 'U Tay', 'U can', 'U panna', 'Anku'];
+
+
+
+// for loop 
+for (var i = 0; i < students.length; i++) {
+  
+  console.log('Great to see you, ' + students[i] + '!');
+}
